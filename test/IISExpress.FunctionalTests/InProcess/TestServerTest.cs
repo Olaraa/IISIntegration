619f17504f88474154c70139d6ee7570ed1d16c9
Pavel Krymets <pavel@krymets.com>
2018-07-13 15:34:40 -0700

    Cancel write

--- test/IISExpress.FunctionalTests/InProcess/TestServerTest.cs
+++ test/IISExpress.FunctionalTests/InProcess/TestServerTest.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
 using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.Logging.Testing;
 using Xunit;
@@ -44,18 +45,17 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
         [ConditionalFact]
         public async Task WritesSucceedAfterClientDisconnect()
         {
-            TaskCompletionSource<bool> requestStartedCompletionSource =
-                new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-            TaskCompletionSource<bool> requestCompletedCompletionSource =
-                new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestStartedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var clientDisconnectedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestCompletedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
 
             var data = new byte[1024];
             using (var testServer = await TestServer.Create(
                 async ctx =>
                 {
                     requestStartedCompletionSource.SetResult(true);
-
-                    for (int i = 0; i < 1000; i++)
+                    await clientDisconnectedCompletionSource.Task;
+                    for (var i = 0; i < 1000; i++)
                     {
                         await ctx.Response.Body.WriteAsync(data);
                     }
@@ -65,16 +65,10 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
             {
                 using (var connection = testServer.CreateConnection())
                 {
-                    await connection.Send(
-                        "POST / HTTP/1.1",
-                        "Content-Length: 1",
-                        "Host: localhost",
-                        "Connection: close",
-                        "",
-                        "");
-
+                    await SendContentLength1Post(connection);
                     await requestStartedCompletionSource.Task.TimeoutAfterDefault();
                 }
+                clientDisconnectedCompletionSource.SetResult(true);
 
                 await requestCompletedCompletionSource.Task.TimeoutAfterDefault();
             }
@@ -83,8 +77,8 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
         [ConditionalFact]
         public async Task ReadThrowsAfterClientDisconnect()
         {
-            TaskCompletionSource<bool> requestStartedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-            TaskCompletionSource<bool> requestCompletedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestStartedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestCompletedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
 
             Exception exception = null;
 
@@ -106,14 +100,7 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
             {
                 using (var connection = testServer.CreateConnection())
                 {
-                    await connection.Send(
-                        "POST / HTTP/1.1",
-                        "Content-Length: 1",
-                        "Host: localhost",
-                        "Connection: close",
-                        "",
-                        "");
-
+                    await SendContentLength1Post(connection);
                     await requestStartedCompletionSource.Task.TimeoutAfterDefault();
                 }
 
@@ -125,13 +112,54 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
         }
 
         [ConditionalFact]
-        public async Task ReaderThrowsCancelledException()
+        public async Task WriterThrowsCancelledException()
         {
-            TaskCompletionSource<bool> requestStartedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-            TaskCompletionSource<bool> requestCompletedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestStartedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestCompletedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
 
             Exception exception = null;
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var data = new byte[1024];
+            using (var testServer = await TestServer.Create(async ctx =>
+            {
+                requestStartedCompletionSource.SetResult(true);
+                try
+                {
+                    while (true)
+                    {
+                        await ctx.Response.Body.WriteAsync(data, cancellationTokenSource.Token);   
+                    }
+                }
+                catch (Exception e)
+                {
+                    exception = e;
+                }
+
+                requestCompletedCompletionSource.SetResult(true);
+            }, LoggerFactory))
+            {
+                using (var connection = testServer.CreateConnection())
+                {
+                    await SendContentLength1Post(connection);
+
+                    await requestStartedCompletionSource.Task.TimeoutAfterDefault();
+                    cancellationTokenSource.Cancel();
+                    await requestCompletedCompletionSource.Task.TimeoutAfterDefault();
+                }
+
+                Assert.IsType<OperationCanceledException>(exception);
+            }
+        }
+
+        [ConditionalFact]
+        public async Task ReaderThrowsCancelledException()
+        {
+            var requestStartedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var requestCompletedCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            Exception exception = null;
+            var cancellationTokenSource = new CancellationTokenSource();
 
             var data = new byte[1024];
             using (var testServer = await TestServer.Create(async ctx =>
@@ -151,20 +179,24 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
             {
                 using (var connection = testServer.CreateConnection())
                 {
-                    await connection.Send(
-                        "POST / HTTP/1.1",
-                        "Content-Length: 1",
-                        "Host: localhost",
-                        "Connection: close",
-                        "",
-                        "");
-
+                    await SendContentLength1Post(connection);
                     await requestStartedCompletionSource.Task.TimeoutAfterDefault();
                     cancellationTokenSource.Cancel();
                     await requestCompletedCompletionSource.Task.TimeoutAfterDefault();
                 }
                 Assert.IsType<OperationCanceledException>(exception);
             }
+        }
+
+        private static async Task SendContentLength1Post(TestConnection connection)
+        {
+            await connection.Send(
+                "POST / HTTP/1.1",
+                "Content-Length: 1",
+                "Host: localhost",
+                "Connection: close",
+                "",
+                "");
         }
     }
 }
